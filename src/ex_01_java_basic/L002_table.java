824eb20ad27699598cceacc76b7b307cce75be8c
nishu <nishubpt1@gmail.com>
2025-03-12 13:17:36 +0530

    11march

---
+++ src/ex_01_java_basic/L002_table.java
@@ -0,0 +1,17 @@
+package ex_01_java_basic;
+
+public class L002_table {
+    public static void main(String[] args) {
+
+        System.out.println("9x1="+9*1);
+        System.out.println("9x2="+9*2);
+        System.out.println("9x3="+9*3);
+        System.out.println("9x4="+9*4);
+        System.out.println("9x5="+9*5);
+        System.out.println("9x6="+9*6);
+        System.out.println("9x7="+9*7);
+        System.out.println("9x8="+9*8);
+        System.out.println("9x9="+9*9);
+        System.out.println("9x10="+9*10);
+    }
+}
